2081720e14c7708abf104c327e1bb93fe90f64c5
Marko Dragicevic <mdragicevic58@gmail.com>
2022-01-21 15:02:58 +0100

    Gotov front i back za registraciju i login

---
+++ WP-DrustvenaMreza/src/rest/SparkMain.java
@@ -0,0 +1,61 @@
+package rest;
+
+import static spark.Spark.staticFiles;
+import static spark.Spark.port;
+import static spark.Spark.path;
+import static spark.Spark.get;
+import static spark.Spark.post;
+import static spark.Spark.put;
+
+import java.io.File;
+import java.io.IOException;
+
+import com.google.gson.Gson;
+
+import beans.Korisnik;
+import dao.KomentariDAO;
+import dao.KorisnikDAO;
+import dao.ObjaveDAO;
+import dao.SlikeDAO;
+import dao.ZahteviDAO;
+import spark.Session;
+
+public class SparkMain {
+	static String dataPath = "./static/data";
+	private static SlikeDAO slikeDAO = new SlikeDAO(dataPath);
+	private static KorisnikDAO korisniciDAO = new KorisnikDAO(dataPath, slikeDAO);
+	private static KomentariDAO komentariDAO = new KomentariDAO(dataPath, korisniciDAO);
+	private static ObjaveDAO objaveDAO = new ObjaveDAO(dataPath, korisniciDAO, komentariDAO);
+	private static ZahteviDAO zahteviDAO = new ZahteviDAO(dataPath, korisniciDAO);
+
+	private static Gson g = new Gson();
+
+	public static void main(String[] args) throws IOException {
+		port(8080);
+		staticFiles.externalLocation(new File("./static").getCanonicalPath());
+
+		get("/rest/test", (req, res) -> {
+			return "radi";
+		});
+
+		post("rest/korisnici/login", (req, res) -> {
+			res.type("application/json");
+			String kIme = req.queryParams("logKorisnicko");
+			String lozinka = req.queryParams("logPass");
+			Session ss = req.session(true);
+			Korisnik k = KorisniciService.login(kIme, lozinka, korisniciDAO);
+			if (k != null) {
+				ss.attribute("ulogovaniKorisnik", k);
+				return g.toJson(k);
+			} else {
+				return false;
+			}
+		});
+
+		post("/rest/korisnici/register", (req, res) -> {
+			res.type("application/json");
+			Korisnik k = g.fromJson(req.body(), Korisnik.class);
+			return g.toJson(KorisniciService.register(k, korisniciDAO));
+		});
+	}
+}
---
+++ WP-DrustvenaMreza/static/js/loginReg.js
@@ -0,0 +1,136 @@
+$(document).ready(function(){
+    $(':input')
+        .not(':checkbox, :radio, select').val('');
+
+    $(".login").hide();
+    $(".register_li").addClass("active");
+
+    $(".login_li").click(function () {
+        $(this).addClass("active");
+        $(".register_li").removeClass("active");
+        $(".login").show(300);
+        $(".register").hide(300);
+    });
+
+    $(".register_li").click(function () {
+        $(this).addClass("active");
+        $(".login_li").removeClass("active");
+        $(".register").show(300);
+        $(".login").hide(300);
+    });
+
+    $(".register .btn").click(function () {
+        register();
+    });
+
+    $(".login .btn").click(function () {
+        login();
+    });
+});
+
+function register() {
+    
+    let kIme = $('input[name="username"]').val();
+    let email = $('input[name="email"]').val();
+    let lozinka = $('input[name="password"]').val();
+    let ponovljenaLozinka = $('input[name="repPassword"]').val();
+    let ime = $('input[name="name"]').val();
+    let prezime = $('input[name="lastName"]').val();
+    let pol = $('input[name="gender"]:checked').val();
+    
+    let greska = false;
+
+    if (!kIme) {
+        $('input[name="username"]').addClass('error');
+        $('input[name="username"]').attr("placeholder", "Enter username");
+        greska = true;
+    }
+    if (!email) {
+        $('input[name="email"]').addClass('error');
+        $('input[name="email"]').attr("placeholder", "Enter e-mail");
+        greska = true;
+    }
+    if (!lozinka) {
+        $('input[name="password"]').addClass('error');
+        $('input[name="password"]').attr("placeholder", "Enter password");
+        greska = true;
+    }
+    if (!ponovljenaLozinka) {
+        $('input[name="repPassword"]').addClass('error');
+        $('input[name="repPassword"]').attr("placeholder", "Re-enter password");
+        greska = true;
+    }
+    if (lozinka != ponovljenaLozinka) {
+        $('input[name="repPassword"]').addClass('error');
+        $('input[name="repPassword"]').val('');
+        $('input[name="repPassword"]').attr("placeholder", "Passwords do not match!");
+        greska = true;
+    }
+    if (!ime) {
+        $('input[name="name"]').addClass('error');
+        $('input[name="name"]').attr("placeholder", "Enter name");
+        greska = true;
+    }
+    if (!prezime) {
+        $('input[name="lastName"]').addClass('error');
+        $('input[name="lastName"]').attr("placeholder", "Enter last name");
+        greska = true;
+    }
+
+    if (greska) return;
+    var data = JSON.stringify({
+        korisnickoIme: kIme,
+        lozinka: lozinka,
+        email: email,
+        ime: ime,
+        prezime: prezime,
+        pol: pol
+    });
+
+    $.ajax({
+        type: "POST",
+        url: "/rest/korisnici/register",
+        data: data,
+        contentType: 'application/json',
+        success: function (korisnik) {
+            if (!korisnik)
+                alert("Korisnik vec registrovan");
+            else
+                window.location = "pocetna.html";
+        }
+    });
+}
+
+function login() {
+    let korisnickoIme = $('input[name="logKorisnicko"]').val();
+    let lozinka = $('input[name="logPass"]').val();
+    let greska = false;
+
+    if (!korisnickoIme) {
+        $('input[name="logKorisnicko"]').addClass('error');
+        $('input[name="logKorisnicko"]').attr("placeholder", "Enter username");
+        greska = true;
+
+    }
+    if (!lozinka) {
+        $('input[name="logPass"]').addClass('error');
+        $('input[name="logPass"]').attr("placeholder", "Enter password");
+        greska = true;
+    }
+    if (greska) return;
+    
+    var data = $('#loginForm').serialize();
+    $.ajax({
+        type: "POST",
+        url: '/rest/korisnici/login',
+        data: data,
+        success: function (korisnik) {
+            $('#successLog').text('Successfully logged in!');
+            $('#successLog').show().delay(3000).fadeOut();
+            window.location = "pocetna.html";
+        },
+        fail: function () {
+            alert('ne mere');
+        }
+    });
+}
