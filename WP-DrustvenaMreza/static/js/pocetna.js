20ed1d943d845d5b715bb9f81c57877ed6d3b52e
Marko Dragicevic <mdragicevic58@gmail.com>
2022-01-24 21:06:28 +0100

    uradjeni prijatelji, zahtevi i poruke na frontu

--- WP-DrustvenaMreza/static/js/pocetna.js
+++ WP-DrustvenaMreza/static/js/pocetna.js
@@ -2,63 +2,63 @@ let ulogovaniKorisnik = null;
 
 $(document).ready(function () {
 
-    $(".requests").hide();
-    $(".messages").hide();
-    $(".logout").hide();
-    $(".myprofile_li").addClass("active");
-    let activeDiv = $(".myprofile");
-    let activeLi = $(".myprofile_li");
+    // $(".requests").hide();
+    // $(".messages").hide();
+    // $(".logout").hide();
+    // $(".myprofile_li").addClass("active");
+    // let activeDiv = $(".myprofile");
+    // let activeLi = $(".myprofile_li");
 
-    $(".requests_li").click(function () {
-        if (activeLi.is(this)) {
-            return;
-        }
-        $(this).addClass("active");
-        $(activeLi).removeClass("active");
-        activeLi = $(this);
-        $(".requests").show(300);
-        $(activeDiv).hide(300);
-        activeDiv = $(".requests");
-    });
+    // $(".requests_li").click(function () {
+    //     if (activeLi.is(this)) {
+    //         return;
+    //     }
+    //     $(this).addClass("active");
+    //     $(activeLi).removeClass("active");
+    //     activeLi = $(this);
+    //     $(".requests").show(300);
+    //     $(activeDiv).hide(300);
+    //     activeDiv = $(".requests");
+    // });
 
-    $(".messages_li").click(function () {
-        if (activeLi.is(this)) {
-            return;
-        }
-        $(this).addClass("active");
-        $(activeLi).removeClass("active");
-        activeLi = $(this);
+    // $(".messages_li").click(function () {
+    //     if (activeLi.is(this)) {
+    //         return;
+    //     }
+    //     $(this).addClass("active");
+    //     $(activeLi).removeClass("active");
+    //     activeLi = $(this);
 
-        $(".messages").show(300);
-        $(activeDiv).hide(300);
-        activeDiv = $(".messages");
-    });
+    //     $(".messages").show(300);
+    //     $(activeDiv).hide(300);
+    //     activeDiv = $(".messages");
+    // });
 
-    $(".logout_li").click(function () {
-        if (activeLi.is(this)) {
-            return;
-        }
-        $(this).addClass("active");
-        $(activeLi).removeClass("active");
-        activeLi = $(this);
+    // $(".logout_li").click(function () {
+    //     if (activeLi.is(this)) {
+    //         return;
+    //     }
+    //     $(this).addClass("active");
+    //     $(activeLi).removeClass("active");
+    //     activeLi = $(this);
 
-        $(".logout").show(300);
-        $(activeDiv).hide(300);
-        activeDiv = $(".logout");
-    });
+    //     $(".logout").show(300);
+    //     $(activeDiv).hide(300);
+    //     activeDiv = $(".logout");
+    // });
 
-    $(".myprofile_li").click(function () {
-        if (activeLi.is(this)) {
-            return;
-        }
-        $(this).addClass("active");
-        $(activeLi).removeClass("active");
-        activeLi = $(this);
+    // $(".myprofile_li").click(function () {
+    //     if (activeLi.is(this)) {
+    //         return;
+    //     }
+    //     $(this).addClass("active");
+    //     $(activeLi).removeClass("active");
+    //     activeLi = $(this);
 
-        $(".myprofile").show(300);
-        $(activeDiv).hide(300);
-        activeDiv = $(".myprofile");
-    });
+    //     $(".myprofile").show(300);
+    //     $(activeDiv).hide(300);
+    //     activeDiv = $(".myprofile");
+    // });
 
 
     $.ajax({
